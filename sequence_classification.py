051f3cac8c9ba23ec24fa11a0116fd461342a09b
Zach Mueller <muellerzr@gmail.com>
2023-10-23 21:46:43 +0000

    Time

--- sequence_classification.py
+++ sequence_classification.py
@@ -61,12 +61,14 @@ print("Creating training arguments (weights are stored at `results/sequence_clas
 training_args = TrainingArguments(
     output_dir="results/sequence_classification",  # Where weights are stored
     learning_rate=2e-5,  # The learning rate during training
-    per_device_train_batch_size=16,  # Number of samples per batch during training
-    per_device_eval_batch_size=16,  # Number of samples per batch during evaluation
-    num_train_epochs=2,  # How many iterations through the dataloaders should be done
+    per_device_train_batch_size=32,  # Number of samples per batch during training
+    # per_device_eval_batch_size=32,  # Number of samples per batch during evaluation
+    max_steps=5,  # How many iterations through the dataloaders should be done
     weight_decay=0.01,  # Regularization penalization
-    evaluation_strategy="epoch",  # How often metrics on the evaluation dataset should be computed
-    save_strategy="epoch",  # When to try and save the best model (such as a step number or every iteration)
+    # evaluation_strategy="steps",  # How often metrics on the evaluation dataset should be computed
+    # save_strategy="steps",  # When to try and save the best model (such as a step number or every iteration)
+    # eval_steps=5,
+    report_to="tensorboard",  # Where to report metrics (such as to TensorBoard)
 )
 
 # Create the `Trainer`, passing in the model and arguments
@@ -86,10 +88,15 @@ trainer = Trainer(
 print("Training...")
 trainer.train()
 
+# Push to hub
+print("Pushing to hub...")
+kwargs = {"finetuned_from": model_name, "tasks": "text-classification"}
+trainer.push_to_hub(**kwargs)
+        
 # Performing inference
 text = "This was a masterpiece. Not completely faithful to the books, but enthralling from beginning to end. Might be my favorite of the three."
 # We need to tokenize the inputs and turn them to PyTorch tensors
-encoded_input = tokenizer(text, return_tensors="pt")
+encoded_input = tokenizer(text, return_tensors="pt").to("cuda")
 
 # Then we can perform raw torch inference:
 print("Performing inference...")
